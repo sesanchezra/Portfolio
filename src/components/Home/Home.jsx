55fc13e21cdbbab580af5a135c9a6db91eef1043
Sebastian Sanchez Ramirez <100661054+sesanchezra@users.noreply.github.com>
2022-11-01 21:03:05 -0500

    Home 1.0.1 Home description 15%

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import './Home.css'
+import Profile from '../../assets/Profile.png'
 
 const Home = () => {
     return (
         <div className='Home'>
             <div className='Home__description'>
                 <div className='Home__description__header'>
-                    <h4>Hello, I'm</h4>
-                    <h2>SEBASTIAN</h2>
+                    <h5>Hello, I'm</h5>
+                    <h2>
+                        SEBASTIAN
+                    </h2>
                 </div>
+                <div className='Home__description__content'>
+                    <p>
+                        I`m frontend developer, with pasion to impact the world with simple and effective solutions, that people could love.
+                    </p>
+                </div>
+            </div>
+            <div className='Home__img'>
+                <img src={Profile} alt="profile" />
             </div>
         </div>
     )
